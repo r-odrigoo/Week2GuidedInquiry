6b1a84063f343b4c491f46ffbfcb98ee8c3737ca
r-odrigoo <86336274+r-odrigoo@users.noreply.github.com>
2021-07-07 23:57:08 -0400

    Add files via upload

---
+++ Easy.py
@@ -0,0 +1,11 @@
+def introduction(key):
+    studentNames = {
+    'Rodrigo Cornejo' : 19,
+    'Leland Capps' : 22, 
+    'Reggie Johnson' : 23,
+    }
+    return print(f'Hello, my name is {key} and I am {studentNames[key]} years old.')
+
+
+introduction('Leland Capps')
+
